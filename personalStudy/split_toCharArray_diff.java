cc92508b0c00da273f438f096ea97f706c5c5fcf
ChanIk Yun <ychoik0818@naver.com>
2025-08-09 16:22:05 +0900

    split과 toCharArray 배열 담을 때 차이

---
+++ personalStudy/split_toCharArray_diff.java
@@ -0,0 +1,34 @@
+
+class split{
+
+
+
+}
+
+
+public class split_toCharArray_diff {
+
+    public static void main(String[] args) {
+
+        String str = "Hello";
+        //한 글자씩 string타입으로 잘라준다.
+        String[] arr = str.split(""); // 빈 문자열 기준으로 나누기
+
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.println(arr[i]);
+        }
+
+
+
+        //한 글자씩 char 타입으로 잘라준다.
+        char[] arr2 = str.toCharArray();
+
+        for (char c : arr2) {
+            System.out.println(c);
+        }
+
+
+
+    }
+}
