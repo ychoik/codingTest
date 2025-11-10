6cc9f2bc1256249e4d8bf0b91ab2f58f56fc18a7
ChanIk Yun <ychoik0818@naver.com>
2025-11-10 23:56:20 +0900

    링크 추가 및 수정

--- personalStudy/bfs_boj1012_2.java
+++ personalStudy/bfs_boj1012_2.java
@@ -1,3 +1,5 @@
+//https://www.acmicpc.net/problem/1012
+
 
 import java.util.*;
 public class bfs_boj1012_2 {
@@ -5,15 +7,15 @@ public class bfs_boj1012_2 {
     static int [] dx = {1,-1,0,0};
     static int [] dy = {0,0,1,-1};
 
-    public static void main(String args){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         StringBuilder out = new StringBuilder();
 
         int T = sc.nextInt(); // 테스트 개수
 
         while(T-->0) {
-            int M = sc.nextInt();//배추밭의 가로 길이
-            int N = sc.nextInt();//배추밭의 세로 길이
+            int M = sc.nextInt();//배추밭의 가로
+            int N = sc.nextInt();//배추밭의 세로
             int K = sc.nextInt();//배추가 심어져 있는 위치의 개수
 
             int x = 0, y = 0; //배추의 위치
@@ -25,7 +27,7 @@ public class bfs_boj1012_2 {
                 x = sc.nextInt();//(0<x<M-1)
                 y = sc.nextInt();//(0<Y<N-1)
 
-                if (x < 0 || y < 0 || x >= N || y >= M) {
+                if (x < 0 || y < 0 || x >= M || y >= N) {
                     i--;
                     continue;
                 }
@@ -37,7 +39,7 @@ public class bfs_boj1012_2 {
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < M; j++) {
                     if (field[i][j] == 1 && !visited[i][j]) {
-                        bfs(field, visited, x, y, M, N);
+                        bfs(field, visited, j, i, M, N);
                         worms++;
                     }
                 }
@@ -47,25 +49,25 @@ public class bfs_boj1012_2 {
         }
         System.out.print(out.toString());
     }
-    public static void bfs(int [][] field, boolean[][] visited, int x, int y,  int M, int N)
+    public static void bfs(int [][] field, boolean[][] visited, int sx, int sy,  int M, int N)
     {
         List<int []>array = new ArrayList<>();
         int head=0;
-        array.add(new int[]{x,y});
-        visited[y][x]=true;
+        array.add(new int[]{sx,sy});
+        visited[sy][sx]=true;
 
         while(head<array.size())
         {
             int cur [] = array.get(head++);
-            x=cur[0];
-            y=cur[1];
+            int x=cur[0];
+            int y=cur[1];
 
             for(int d=0; d<4; d++)
             {
                 int nx = x + dx[d];
                 int ny = y + dy[d];
 
-                if(nx<0 || ny<0 || nx>=N || ny>=M) continue;
+                if(nx<0 || ny<0 || nx>=M || ny>=N) continue;
                 if(visited[ny][nx]) continue;
                 if(field[ny][nx]==0) continue;
 
