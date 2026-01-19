c60edf4ed3477fa9a32f5a28c32ae50a4c269442
ChanIk Yun <ychoik0818@naver.com>
2026-01-19 14:56:48 +0900

    문제 번호 수정

--- BOJ/boj_2179_gold4.java
+++ BOJ/boj_2179_gold4.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashMap;
 
-public class boj_2197_gold4 {
+public class boj_2179_gold4 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
