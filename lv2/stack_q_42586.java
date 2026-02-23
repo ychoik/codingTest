2e56f9d676d1e09e406a25753dd289660141d54f
ChanIk Yun <ychoik0818@naver.com>
2026-02-24 00:39:33 +0900

    프로그래머스 고득점 kit lv2 기능개발

---
+++ lv2/stack_q_42586.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class stack_q_42586 {
+    public int[] solution(int[] progresses, int[] speeds) {
+
+        int remain[] = new int[progresses.length];
+        int days[] = new int[progresses.length];
+
+        for(int i=0; i<progresses.length; i++)
+        {
+            remain [i] = 100-progresses[i];
+            if(remain[i]%speeds[i]!=0)
+            {
+                days[i]=remain[i]/speeds[i]+1;
+            }
+            else{
+                days[i]=remain[i]/speeds[i];
+            }
+        }
+        //System.out.println(Arrays.toString(days));
+
+        int str = days[0];//기준일
+        int cnt=1;
+        List<Integer> list = new ArrayList<>();
+        for(int i=1; i<days.length; i++)
+        {
+            if(days[i]<=str)
+            {
+                cnt++;
+            }
+            else{
+                list.add(cnt);
+                str=days[i];
+                cnt=1;
+            }
+
+        }
+
+        list.add(cnt);
+
+        int[] answer = new int[list.size()];
+        for(int i=0; i<answer.length; i++)
+        {
+            answer[i]=list.get(i);
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        stack_q_42586 sol = new stack_q_42586();
+
+        int[] progresses = {93, 30, 55};
+        int[] speeds = {1, 30, 5};
+
+        int[] result = sol.solution(progresses, speeds);
+        System.out.println(Arrays.toString(result)); // [2, 1]
+    }
+
+}
