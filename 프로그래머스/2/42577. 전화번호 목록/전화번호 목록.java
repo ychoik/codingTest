e05d54e1fa5f6706703e70949aa6fcabbda81143
ychoik <143485223+ychoik@users.noreply.github.com>
2026-03-05 21:29:55 +0900

    [level 2] Title: 전화번호 목록, Time: 460.07 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,26 +4,14 @@ class Solution {
         boolean answer = true;
         Arrays.sort(phone_book);
         
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            
-            if (phone_book[i].length() <= phone_book[i + 1].length() &&
-                phone_book[i + 1].substring(0, phone_book[i].length()).equals(phone_book[i])) {
-                return false; 
+        for(int i=0; i<phone_book.length-1; i++)
+        {
+            if(phone_book[i+1].startsWith(phone_book[i]))
+            {
+                return false;
             }
         }
         
-        return true; 
-    }
-
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        String[] phone_book = {"119", "97674223", "1195524421"};
-        boolean result = sol.solution(phone_book);
-        
-        
-        StringBuilder sb = new StringBuilder();
-        sb.append(result);
-        
-        System.out.println(sb);
+        return answer;
     }
 }
