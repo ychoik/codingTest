38f59347cbf0a3cfb80deaf8eba8a54e90f19772
ychoik <143485223+ychoik@users.noreply.github.com>
2026-03-06 21:28:35 +0900

    [Silver V] Title: 기상캐스터, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		
+		int H = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		String input [][] = new String [H][W];
+		
+		for(int i=0; i<H; i++)
+		{
+			String line = br.readLine();
+			int lastCloud = -1;
+
+			for(int j=0; j<W; j++)
+			{
+				char ch = line.charAt(j);
+				int ans;
+				
+				if(ch=='c')
+				{
+					lastCloud=j;
+					ans=0;
+				}
+				else {
+					ans=(lastCloud == -1) ? -1 : (j-lastCloud);
+				}
+				
+				sb.append(ans);
+				if(j<W-1) {
+					sb.append(' ');
+				}
+			}
+			if(i<H-1) {
+				sb.append("\n");
+			}
+			
+		}
+		
+		
+		System.out.println(sb.toString());
+		
+
+
+	}
+
+}
