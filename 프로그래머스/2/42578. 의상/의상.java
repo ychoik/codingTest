fe80d7d823130d3ecc40ffce0c991bcd8c36b00a
ychoik <143485223+ychoik@users.noreply.github.com>
2026-03-11 01:03:20 +0900

    [level 2] Title: 의상, Time: 0.25 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+        
+        
+        for (int count : map.values()) {
+            answer *= (count + 1);
+        }
+        
+        //System.out.println(map.size());
+        
+        
+        
+        return answer-1;
+    }
+}
