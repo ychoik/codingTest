b065ca4e58fabeb5ccceedc2ace209d9fa0aefbf
Chan Ik Yoon <ychoik0818@naver.com>
2026-04-08 01:52:52 +0900

    dfs_basic 오류부분 수정

--- personalStudy/dfs_basic.java
+++ personalStudy/dfs_basic.java
@@ -1,3 +1,26 @@
+/*
+        1. DFS 문제
+        문제: 안전 구역의 개수
+
+        N x M 크기의 지도가 주어진다.
+        지도에는 0과 1이 들어있다.
+
+        1은 땅
+        0은 물
+
+        상하좌우로 연결된 땅들을 하나의 안전 구역이라고 할 때,
+        전체 안전 구역의 개수를 구하시오.
+
+        입력 예시
+        4 5
+        1 1 0 0 1
+        0 1 0 1 1
+        0 0 0 0 0
+        1 1 0 1 0
+
+        출력 예시
+        4
+ */
 import java.util.*;
 import java.io.*;
 public class dfs_basic {
@@ -34,6 +57,7 @@ public class dfs_basic {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         map = new int[N][M];
+        visited = new boolean[N][M];
         for(int i=0; i<N; i++)
         {
             st = new StringTokenizer(br.readLine());
@@ -57,7 +81,7 @@ public class dfs_basic {
             }
         }
 
-
+        System.out.println(cnt);
 
 
     }
