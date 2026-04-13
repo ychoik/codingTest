7ec30879fb6374ccae10716c1a7ca5b4e22d4be3
Chan Ik Yoon <ychoik0818@naver.com>
2026-04-13 23:21:44 +0900

    dp기초 에러 수정

--- personalStudy/boj_1003.java
+++ personalStudy/boj_1003.java
@@ -19,7 +19,7 @@ public class boj_1003 {
         for(int i=2; i<=40; i++)
         {
             dp[i][0] = dp[i-1][0] + dp[i-2][0];
-            dp[i][1] = dp[i-1][1] + dp[i-1][1];
+            dp[i][1] = dp[i-1][1] + dp[i-2][1];
         }
 
         for(int i=0; i<T; i++)
@@ -28,6 +28,6 @@ public class boj_1003 {
             sb.append(dp[n][0]).append(" ").append(dp[n][1]).append("\n");
 
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
