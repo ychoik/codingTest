4a6fab36967ab26f7ee6bece9567346c7a396fdc
Chan Ik Yoon <ychoik0818@naver.com>
2026-04-15 23:57:16 +0900

    백준 1074 골드 5 풀이

---
+++ personalStudy/bfs_1074.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+public class bfs_1074 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int size = 1<<N;
+        int answer = 0;
+
+        while(size > 1)
+        {
+            int half = size/2;
+            int area = half*half;
+
+            if(r<half && c<half){
+
+            }
+            else if(r<half && c>=half){
+                answer += area;
+                c-=half;
+            }
+            else if(r>=half && c<half)
+            {
+                answer+=area*2;
+                r-=half;
+            }
+            else
+            {
+                answer+=area*3;
+                r-=half;
+                c-=half;
+            }
+            size = half;
+
+        }
+        System.out.println(answer);
+
+    }
+}
