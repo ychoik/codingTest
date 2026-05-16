8aaa3ebf156da31dcb82e70a2ae42427b989992a
윤찬익 <ychoik0818@naver.com>
2026-05-16 15:56:21 +0900

    [level 2] Title: 전화번호 목록, Time: 178.06 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,7 +2,7 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        Arrays.sort(phone_book);
+        Arrays.sort(phone_book); //사전 순으로 정렬   
         
         for(int i=0; i<phone_book.length-1; i++)
         {
