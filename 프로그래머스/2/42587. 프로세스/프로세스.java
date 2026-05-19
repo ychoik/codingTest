799a9379aecc74e6e7ae77997d79fcd620ed6e3c
윤찬익 <ychoik0818@naver.com>
2026-05-19 17:30:58 +0900

    [level 2] Title: 프로세스, Time: 1.10 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -6,24 +6,22 @@ class Solution {
         
         for(int i=0; i<priorities.length; i++)
         {
-            q.offer(new int[]{priorities[i], i});//우선순위, 원래 위치 형태로 저장
-        }
+            q.offer(new int[]{priorities[i],i});
+        }  
         
         while(!q.isEmpty())
         {
             int [] current = q.poll();
-            
             boolean hasHigher = false;
             
-            //현재 꺼낸 프로세스보다 우선순위가 높은 게 있는 지 확인
-            for(int [] process:q)
+            for(int [] process : q)
             {
                 if(process[0]>current[0])
                 {
                     hasHigher = true;
                     break;
                 }
-            }            
+            }
             
             if(hasHigher){
                 q.offer(current);
@@ -31,14 +29,12 @@ class Solution {
             else{
                 answer++;
                 
-                if(current[1] == location)
-                {
+                if(current[1] == location){
                     return answer;
                 }
             }
             
         }
-        
         
         
         return answer;
