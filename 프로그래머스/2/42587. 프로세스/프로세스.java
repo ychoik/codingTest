572bc5c15fc2ba524e4af9b70ea3e0b3aae62d93
윤찬익 <ychoik0818@naver.com>
2026-05-20 18:00:33 +0900

    [level 2] Title: 프로세스, Time: 1.61 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -3,36 +3,36 @@ class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
         Deque<int []> q = new ArrayDeque<>();
+        int len = priorities.length;
         
-        for(int i=0; i<priorities.length; i++)
+        for(int i=0; i<len; i++)
         {
-            q.offer(new int[]{priorities[i],i});
-        }  
+            q.offer(new int []{priorities[i], i});
+        }
         
-        while(!q.isEmpty())
-        {
-            int [] current = q.poll();
-            boolean hasHigher = false;
+        while(!q.isEmpty()){
+            int []current = q.poll();
+            boolean higher = false;
             
-            for(int [] process : q)
+            for(int []process : q)
             {
                 if(process[0]>current[0])
                 {
-                    hasHigher = true;
+                    higher = true;
                     break;
                 }
             }
             
-            if(hasHigher){
+            if(higher){
                 q.offer(current);
             }
             else{
                 answer++;
-                
                 if(current[1] == location){
                     return answer;
                 }
             }
+            
             
         }
         
