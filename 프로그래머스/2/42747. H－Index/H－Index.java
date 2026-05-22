460ca0c03040f203a0ac8c9e3d5a0b0e8ffdf2c9
윤찬익 <ychoik0818@naver.com>
2026-05-22 16:12:46 +0900

    [level 2] Title: H-Index, Time: 0.61 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        int n = citations.length;
+        
+        for(int i=0; i<n; i++)
+        {
+            int h = n-i;
+            if(citations[i]>=h){
+                return h;
+            }
+        }
+        
+        return answer;
+    }
+}
